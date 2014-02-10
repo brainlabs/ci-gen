5d72b73a71d05c393e6c736611463ae142746f5e
narara <narara84@gmail.com>
2014-02-11 02:55:16 +0700

    add core controller

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,22 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/*
+ *  Copyright 2013
+ * 
+ */
+
+/**
+ * Description of MY_Controller
+ *
+ * @author Daud Simbolon <daud.simbolon@gmail.com>
+ */
+class MY_Controller extends CI_Controller
+{
+
+    public function __construct() 
+    {
+        parent::__construct();
+    }
+
+}
+
+?>
