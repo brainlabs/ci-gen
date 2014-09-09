79a1807cab2e31f03990e5bbc15e193dfb8b1995
brainlabs <daud.simbolon@gmail.com>
2014-09-09 16:32:12 +0700

    create example database

--- install/assets/install.sql
+++ install/assets/install.sql
@@ -2,3 +2,29 @@
 -- Paste your SQL dump into this file
 --
 
+CREATE TABLE IF NOT EXISTS `jurnals` (
+  `jurnal_id` int(11) NOT NULL AUTO_INCREMENT,
+  `operator_id` int(11) DEFAULT NULL,
+  `tanggal` date DEFAULT NULL,
+  `jam` time DEFAULT NULL,
+  `uraian` text,
+  `shift` char(4) DEFAULT NULL,
+  PRIMARY KEY (`jurnal_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 ;
+
+
+
+--
+-- Struktur dari tabel `operator`
+--
+
+CREATE TABLE IF NOT EXISTS `operator` (
+  `operator_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(45) DEFAULT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `jabatan_id` int(11) DEFAULT NULL,
+  `no_telepon` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`operator_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8  ;
