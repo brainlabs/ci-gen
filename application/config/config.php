adcc12ed04f2d2343e3f6d43bb2be94bc7aec8a0
brainlabs <daud.simbolon@gmail.com>
2014-09-10 08:50:13 +0700

    modify htaccess & remove index.php on url

--- application/config/config.php
+++ application/config/config.php
@@ -31,7 +31,7 @@ $config['per_page'] = 10;
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
